fdeff16b59cc655b1c26c3d7e88b375cd5158d99
srinivas957 <srinivasmadabushini777@gmail.com>
2024-12-25 01:41:25 -0500

    git learning

---
+++ MultiplyExample.java
@@ -0,0 +1,8 @@
+public class MultiplyExample {
+    public static void main(String[] args) {
+        int a;
+        a = 5;
+        System.out.println("7*7 = " + 7*7);
+        System.out.println("a*5 = " + a*5);
+    }
+}
